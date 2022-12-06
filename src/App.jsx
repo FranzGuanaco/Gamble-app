72f2f446b7fa0fb48c023fec96311205e39384f5
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-12-06 22:31:09 +0100

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello c'est pierre</h1>
+function App () {
+
+    return(
+      <div className="App">
+      <Header />
     </div>
-  );
-}
+    )
+    }
+
 
 export default App;
